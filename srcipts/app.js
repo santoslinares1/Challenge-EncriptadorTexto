bfb5be75b04d3cb2d6e84fda55ec4fc04d132d73
santoslinares1 <slinares@mi.unc.edu.ar>
2024-08-20 16:41:44 -0300

    modificados:     index.html 	modificados:     srcipts/app.js 	modificados:     styles/styles.css 	assets/github.svg 	assets/linkedin.svg

--- srcipts/app.js
+++ srcipts/app.js
@@ -1,1 +1,100 @@
-let texto_encripat = "";
+const textoEncriptar = document.getElementById('textoEncriptar');
+const mostrarTexto = document.getElementById('mostrartexto');
+const encriptar = document.getElementById('encriptar');
+const desencriptar = document.getElementById('desencriptar')
+const indicaciones = document.getElementById('indicaciones');
+const texto = document.getElementById('text');
+const vocalesAcentos = /[áéíóúü]/; 
+const vocalesEncriptadas = ['ai', 'enter', 'imes', 'ober', 'ufat']; 
+let textoEncriptado = '';
+let textoDesencriptado = '';
+
+
+function btnEncriptar(){
+    //Chequea que no haya mayusculas o acentos
+    if (textoEncriptar.value.match(/[A-Z]/) || textoEncriptar.value.match(vocalesAcentos)){
+        alert('El texto contiene mayusculas o acentos');
+    }else{
+        //Chequea que haya texto
+        if (textoEncriptar.value == ""){
+            alert('Ingrese el texto que desea encriptar o desencriptar')
+        }else{
+            texto.removeAttribute('hidden')
+            indicaciones.style.display = 'none'
+            //mostrarTexto.innerHTML = textoEncriptar.value;
+            for (const iterator of textoEncriptar.value) {
+                if (iterator == 'a') {
+                    textoEncriptado += 'ai'
+                } else if (iterator == 'e') {
+                    textoEncriptado += 'enter'
+                } else if (iterator == 'i') {
+                    textoEncriptado += 'imes'
+                }
+                else if (iterator == 'o'){
+                    textoEncriptado += 'ober';
+                }
+                else if(iterator == 'u'){
+                    textoEncriptado += 'ufat';
+                }
+                else{
+                    textoEncriptado += iterator;
+                } 
+            }
+            mostrarTexto.innerHTML = textoEncriptado;
+            texto.style.display = 'flex';
+        }
+
+        }
+
+}
+
+
+function btnDesencriptar(){
+    if(textoEncriptar.value.match(/[A-Z]/) || textoEncriptar.value.match(vocalesAcentos)){
+        alert('El texto contiene mayusculas o acentos')
+    }else{
+        if (textoEncriptar.value == ""){
+            alert('Ingrese el texto que desea encriptar o desencriptar')
+        }else{
+            texto.removeAttribute('hidden')
+            indicaciones.style.display = 'none'
+            texto.style.display = 'flex';
+            let desencriptado = textoEncriptar.value.split(" ").map(Element => {
+                vocalesEncriptadas.forEach(E => {
+                    if (Element.match(E)){
+                        if (E == 'ai'){
+                            Element = Element.replace(E, 'a');
+                        }else if (E == 'enter'){
+                            Element = Element.replace(E, 'e');
+                        }else if (E == 'imes'){
+                            Element = Element.replace(E, 'i');
+                        }else if (E == 'ober'){
+                            Element = Element.replace(E, 'o');
+                        }else if (E == 'ufat'){
+                            Element = Element.replace(E, 'u');
+                        }
+                    }
+                });
+                return Element;
+            }).join(" ");
+            mostrarTexto.innerHTML = desencriptado; 
+        
+        }
+    }
+
+    
+}
+desencriptar.addEventListener('click', btnDesencriptar)
+
+document.getElementById('copiar').addEventListener('click', function() {
+    txtCopy = mostrarTexto.innerText;
+
+    navigator.clipboard.writeText(txtCopy)
+        .then(() => {
+            alert('Texto copiado al portapapeles');
+        })
+        .catch(err => {
+            console.error('Error al copiar: ', err);
+        });
+});
+console.log(textoEncriptado.innerHTML)
